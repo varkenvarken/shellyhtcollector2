0880bdd45810c5337643c8572cbd8f0c8db1e642
varkenvarken <michel@michelanders.nl>
2022-08-06 13:24:03 +0200

    tweaks to testing

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name="shellyhtcollector2",
-    version="0.1.0",
+    name="htcollector",
+    version="0.0.1",
     description="Tools to log updates from Shelly HT devices",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -18,12 +18,14 @@ setup(
     packages=["shellyhtcollector"],
     python_requires=">=3.8",
     install_requires=[
-        "mariadb", "python-dateutil",
+        "mariadb",
+        "python-dateutil",
     ],
     zip_safe=False,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
