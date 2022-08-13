7e91da694e20f2a7081241b59e040a1d32d90ed2
varkenvarken <michel@michelanders.nl>
2022-08-13 13:33:49 +0200

    black

--- htcollector/Server.py
+++ htcollector/Server.py
@@ -17,7 +17,7 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 #
-#  version: 20220813110304
+#  version: 20220813133332
 
 import re
 from io import BytesIO as IO
